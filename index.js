0ca6e3dd43348c304cb3919e905fb0a7dbcdf90b
Sérgio Soares Nunes <serginhossnca@hotmail.com>
2020-01-20 22:29:31 -0300

    Add port as env variable. For post on zeit

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const bodyParser = require('body-parser')
 const sqlite = require('sqlite')
 const dbConnection = sqlite.open('banco.sqlite', {Promise})
 
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true}))
@@ -131,7 +133,7 @@ const init = async() => {
 
 init()
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
     if(err){
         console.log('Não foi possível iniciar o servidor do Jobify!')
     }else{
